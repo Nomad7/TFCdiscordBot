882904da73e66b705edf9b2d413a3e8269957b4b
Nomad7 <enochson1@hotmail.com>
2022-06-23 07:40:44 -0600

    Minor refactor and cleanup

--- TFCmaps.py
+++ TFCmaps.py
@@ -8,41 +8,76 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google.auth.exceptions import RefreshError
 from google.oauth2 import service_account
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google.auth.exceptions import RefreshError
+
 
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# The ID and range of Neon's sheet of maps + team sizes.
-SAMPLE_SPREADSHEET_ID = '1sXUDGC9XrRqKNbjq-qZeMNIgQDeRXdimEY1PZmXt6cY'
-SAMPLE_RANGE_NAME = 'Full List!A3:D'
+# The ID and range of Neon's gsheet of maps and team sizes.
+MAPS_SPREADSHEET_ID = '1sXUDGC9XrRqKNbjq-qZeMNIgQDeRXdimEY1PZmXt6cY'
+# This range includes the following fields:
+# mapname: [name of map]
+# style (not used): ctf|adl|r-ctf|1flag-ctf|cp|fun
+# team sizes: 2v2 through 10v10 depending on map
+# mirvs (not used): recommended mirv class count
+MAPS_RANGE_NAME = 'Full List!A3:D'
+
+# Using a Sheets API service account makes things easier.
+# Remember to share the spreadsheet with the service account.
 secret_file = os.path.join(os.getcwd(), 'SvcAcctCredentials.json')
 
-def pullmaps(players='2'):
-    pulledmaps = [] # i don't think this is necessary
+# Initializing list of maps here
+maps = []
+
+def load_maps():
     creds = None
-    # the file SvcAcctCredentials.json stores service account credentials
-    # the spreadsheet must be shared with this service account
+    # The file SvcAcctCredentials.json stores service account credentials
+    # The spreadsheet must be shared with this service account
     if os.path.exists('SvcAcctCredentials.json'):
         creds = service_account.Credentials.from_service_account_file(secret_file, scopes=SCOPES)
 
-    # if service account isn't set up, try prompting the user
+    # If service account isn't set up, try prompting the user to log in
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
+
     # # Save the credentials for the next run - N/A with service account
+    # https://developers.google.com/sheets/api/quickstart/python
     #     with open('token.json', 'w') as token:
     #         token.write(creds.to_json())
 
-    #check playercount so we can get the right maps
+    # Pull the maps
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API and get the sheet range contents
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=MAPS_SPREADSHEET_ID,
+                                    range=MAPS_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        # If it's empty say so
+        if not values:
+            print('No data found.')
+            return
+   
+    except HttpError as err:
+        print(err)
+    
+    return values
+
+def filter_maps(players='2'):
+    maps_filtered = [] # Initialize empty list
+    # Check playercount so we can get the right maps
     match players:
         case '3':
             playercount = '3v3'
@@ -63,53 +98,43 @@ def pullmaps(players='2'):
         case _:
             playercount = '2v2'
     
-    #pull the maps
+    # Pull map names from column A (0) based on team sizes in column C (2)
     try:
-        service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-
-        #if it's empty say so
-        if not values:
-            print('No data found.')
-            return
-
-        #pull map names from column A (0) based on team sizes in column C (2)
-        for row in values:
+        for row in maps:
             if playercount in row[2]:
-                pulledmaps.append(row[0])
+                maps_filtered.append(row[0])
    
     except HttpError as err:
         print(err)
     
-    return pulledmaps
+    return maps_filtered
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
+# DISCORD BOT STUFF
+# Bot expects !commands
 bot = commands.Bot(command_prefix='!')
 
-    #TODO: track how many players have done !add and give hints about next steps (!maps2 or add more or !maps3 etc)
-    #TODO: automatically newmaps
-    #TODO: some kind of vote tracking/closure system?
+    # TODO: track how many players have done !add and give hints about next steps (!maps2 or add more or !maps3 etc)
+    # TODO: automatically newmaps
+    # TODO: some kind of vote tracking/closure system?
 
-@bot.command(name='maps', help='Returns 3 random maps from neon\'s spreadsheet, use !maps 3 for 3v3, !maps 4 for 4v4 etc')
-async def choose_maps(ctx, players):
-    #populate the maplist from google sheets
-    maplistsheets = pullmaps(players)
+@bot.command(name='test', help='Debugging/helper command because Nomad is kind of dumb.')
+async def test_stuff(ctx):
+    response = ('testing')
+    message = await ctx.send(response)
+
+@bot.command(name='maps', help='Returns 3 random maps from neon\'s spreadsheet, use !maps 3 for 3v3, !maps 4 for 4v4 etc.')
+async def choose_maps(ctx, players='2'):
+    # Filter the maplist based on player count
+    maplistsheets = filter_maps(players)
     
-    #get 3 at random
-    #TODO: make this less than fully random? remember previous pick(s)?
+    # Get 3 at random
+    # TODO: make this less than fully random? remember previous pick(s)?
     mapstochoose = [random.sample(maplistsheets,3)]
     print(mapstochoose)
     
-    #format response
-    #TODO: update results with names of voters?
-    #TODO: automatically newmaps at (playercount) total votes?
+    # Format response and offer a new maps option
+    # TODO: update results with names of voters?
+    # TODO: automatically newmaps at (playercount) total votes?
     response = ( '```'
         '1    ' + mapstochoose[0][0] + '\n'
         '2    ' + mapstochoose[0][1] + '\n'
@@ -118,13 +143,13 @@ async def choose_maps(ctx, players):
     )
     message = await ctx.send(response)
 
-    #add reactions for vote tracking
+    # Add reactions for vote tracking
     emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣']
     for emoji in emojis:
         await message.add_reaction(emoji)
 
 
-#if something bad happens write it down
+# If something bad happens write it down
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
@@ -133,9 +158,19 @@ async def on_error(event, *args, **kwargs):
         else:
             raise
 
-#lemme know when it's ready
+# Bot's secret, see ID https://discord.com/developers/docs/topics/oauth2
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Populate maplist on launch
+maps = load_maps()
+
+# Announce when ready
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord! Now online in these servers...')
+    for i in bot.guilds:
+        print(i)
 
+# Start the bot when this file is running
 bot.run(TOKEN)
